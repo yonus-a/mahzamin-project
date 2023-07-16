48aba0807ee0d03c3423c5f571b629bcc225bb3a
yonus <yonusamire@gmail.com>
2023-07-16 13:27:28 +0330

    feat: add ads one section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@components/hero";
 import ProductsCategorization from "@components/products-categorization";
 import Container from "@components/container";
+import AdsOne from "@components/ads-one";
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <Hero />
       <Container>
         <ProductsCategorization />
+        <AdsOne/>
       </Container>
     </main>
   );
